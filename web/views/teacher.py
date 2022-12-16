329f2f6a3587ce2ab48ce634b20e4bd6439bab4e
Tomas Mensik <tomasmensik2003@seznam.cz>
2022-12-16 11:38:43 +0100

    Filter

--- web/views/teacher.py
+++ web/views/teacher.py
@@ -1,33 +1,50 @@
-import csv
-import dataclasses
-import io
 import itertools
 import os
-import shutil
+import io
+import csv
 import tarfile
 import tempfile
+import re
+import json
+import subprocess
+import datetime
+import shutil
+import logging
 import traceback
-from collections import OrderedDict
-from typing import Dict, List, Tuple
 
+import rq
+import logging
+from shutil import copyfile
+from collections import OrderedDict
+
+from django.db.models import Q
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.models import User
-from django.contrib.contenttypes.models import ContentType
-from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
-from notifications.models import Notification
-from notifications.signals import notify
+from django.utils import timezone as tz
+from django.conf import settings
+from django.db.models import Max, Min, Count
+from django.core.cache import caches
+from django.contrib.contenttypes.models import ContentType
 
-from common.bulk_import import BulkImport, ImportException
-from common.evaluate import evaluate_submit, get_meta
-from common.models import AssignedTask, Class, Subject, Submit, Task, assignedtask_results
-from common.moss import PlagiarismMatch
-from common.utils import is_teacher
+import django_rq
+from notifications.signals import notify
+from notifications.models import Notification
+from unidecode import unidecode
+
+from common.models import Submit, Class, Task, AssignedTask, Subject, assignedtask_results, current_semester_conds, current_semester
 from evaluator.results import EvaluationResult
-from evaluator.testsets import TestSet
 from kelvin.settings import BASE_DIR, MAX_INLINE_CONTENT_BYTES
-from . import statistics
-from .utils import file_response
+from evaluator.testsets import TestSet
+from common.evaluate import get_meta, evaluate_submit
+from common.utils import is_teacher
+from common.moss import enqueue_moss_check, moss_delete_job_from_cache, \
+    moss_job_cache_key, moss_result, \
+    moss_task_set_opts, \
+    moss_task_get_opts
+from common.bulk_import import BulkImport, ImportException
 
 
 @user_passes_test(is_teacher)
@@ -38,51 +55,107 @@ def teacher_task(request, task_id):
     testset = TestSet(task_dir, get_meta(request.user.username))
 
     return render(request, 'web/task_detail.html', {
-        'task': task,
-        'text': testset.load_readme(),
-        'inputs': testset,
-        'max_inline_content_bytes': MAX_INLINE_CONTENT_BYTES,
+          'task': task,
+          'text': testset.load_readme(),
+          'inputs': testset,
+          'max_inline_content_bytes': MAX_INLINE_CONTENT_BYTES,
     })
 
 
-def enrich_matches(matches: List[PlagiarismMatch], teacher: User, task: Task) -> List[
-    Dict[str, str]]:
-    """
-    Converts PlagiarismMatches to dictionaries and adds additional information
-    used by the frontend to them.
-    """
-
-    def get_class_and_link(assignment_id: int, login: str) -> Tuple[str, str]:
-        assignment = AssignedTask.objects.get(pk=assignment_id)
-        clazz = assignment.clazz
-        code = clazz.code
-        semester = clazz.semester
-        class_str = f"{code} ({semester})"
-        link = reverse("find_task_detail", kwargs=dict(task_id=assignment.task.id, login=login))
-        return (class_str, link)
-
+def add_teaching_flag_to_matches(matches, teacher: User, task: Task):
     classes = Class.objects.current_semester().filter(teacher=teacher, assignedtask__task=task)
     students = {v[0] for v in User.objects.filter(students__in=classes).values_list("username")}
-    match_items = []
     for match in matches:
-        match_data = dataclasses.asdict(match)
-        match_data["teaching"] = match.first.login in students or match.second.login in students
-        match_data["first_fullname"] = User.objects.get(
-            username=match.first.login
-        ).get_full_name()
+        match["teaching"] = match["first_login"] in students or match["second_login"] in students
 
-        (first_class, first_link) = get_class_and_link(match.first.assignment_id, match.first.login)
-        match_data["first_class"] = first_class
-        match_data["first_link"] = first_link
-        match_data["second_fullname"] = User.objects.get(
-            username=match.second.login
-        ).get_full_name()
 
-        (second_class, second_link) = get_class_and_link(match.second.assignment_id, match.second.login)
-        match_data["second_class"] = second_class
-        match_data["second_link"] = second_link
-        match_items.append(match_data)
-    return match_items
+def sort_matches(matches):
+    matches.sort(key=lambda v: (
+        v["teaching"],
+        v["lines"],
+        max(v["first_percent"], v["second_percent"])
+    ), reverse=True)
+
+
+@user_passes_test(is_teacher)
+def teacher_task_moss_check(request, task_id):
+    # clear MOSS notifications
+    Notification.objects.filter(
+        action_object_object_id=task_id,
+        recipient_id=request.user.id,
+        verb="plagiarism",
+    ).update(unread=False)
+
+    cache = caches['default']
+    key_job = moss_job_cache_key(task_id)
+
+    task = get_object_or_404(Task, pk=task_id)
+
+    job_id = cache.get(key_job)
+    if job_id:
+        refresh = False
+        try:
+            job = django_rq.jobs.get_job_class().fetch(job_id, connection=django_rq.queues.get_connection())
+            status = job.get_status()
+            if status == "started":
+                refresh = True
+            elif status == "queued":
+                refresh = True
+                status += f" {job.get_position() + 1}"
+            elif status == "failed" and job.exc_info:
+                status += f"\n{job.exc_info}"
+                moss_delete_job_from_cache(task_id)
+        except (rq.exceptions.NoSuchJobError, AttributeError) as e:
+            moss_delete_job_from_cache(task_id)
+            logging.exception(e)
+            status = "unknown"
+        return render(request, "web/moss.html", {
+            "status": status,
+            "task": task,
+            "refresh": refresh
+        })
+
+    if request.method == 'POST':
+        enqueue_moss_check(task_id)
+        return redirect(request.path_info)
+
+    opts = moss_task_get_opts(task_id)
+
+    if 'percent' in request.GET:
+        opts = {
+            **opts,
+            "percent": int(request.GET.get("percent", 5)),
+            "lines": int(request.GET.get("lines", 1)),
+            "show_to_students": int(request.GET.get("show_to_students", False)),
+        }
+        moss_task_set_opts(task_id, opts)
+
+    res = moss_result(task_id, percent=opts['percent'], lines=opts['lines'])
+    if not res:
+        return render(request, 'web/moss.html', {
+            "task": task,
+            "has_result": False
+        })
+    else:
+        newer_submit_count = Submit.objects.filter(
+            assignment__task_id=task.id,
+            created_at__gt=res.started_at
+        ).count()
+        add_teaching_flag_to_matches(res.matches, request.user, task)
+        sort_matches(res.matches)
+        return render(request, 'web/moss.html', {
+            "has_result": True,
+            "success": res.success,
+            "log": res.log,
+            "matches": res.matches,
+            "graph": res.to_svg(anonymize=False),
+            "opts": res.opts,
+            "started_at": res.started_at,
+            "finished_at": res.finished_at,
+            "moss_url": res.url,
+            "newer_submit_count": newer_submit_count,
+            "task": task,
+        })
 
 
 @user_passes_test(is_teacher)
@@ -93,10 +166,7 @@ def submits(request, student_username=None):
         filters['student__username'] = student_username
         student_full_name = get_object_or_404(User, username=student_username).get_full_name()
 
-    submits = Submit.objects.filter(**filters).order_by('-id').select_related('student',
-                                                                              'assignment',
-                                                                              'assignment__task')[
-              :100]
+    submits = Submit.objects.filter(**filters).order_by('-id').select_related('student', 'assignment', 'assignment__task')[:100]
     return render(request, "web/submits.html", {
         'submits': submits,
         'student_username': student_username,
@@ -105,8 +175,7 @@ def submits(request, student_username=None):
 
 
 def get_last_submits(assignment_id):
-    submits = Submit.objects.filter(assignment_id=assignment_id).order_by('-submit_num',
-                                                                          'student_id')
+    submits = Submit.objects.filter(assignment_id=assignment_id).order_by('-submit_num', 'student_id')
 
     result = []
     processed = set()
@@ -129,8 +198,10 @@ def download_assignment_submits(request, assignment_id):
                     tar.add(source.phys, f"{submit.student.username}/{source.virt}")
 
         f.seek(0)
+        response = HttpResponse(f.read(), 'application/tar')
         filename = f"{assignment.task.sanitized_name()}_{assignment.clazz.day}{assignment.clazz.time:%H%M}.tar.gz"
-        return file_response(f, filename, "application/tar")
+        response['Content-Disposition'] = f'attachment; filename="{unidecode(filename)}"'
+        return response
 
 
 def get_assignment_submits(assignment: AssignedTask):
@@ -147,7 +218,7 @@ def get_assignment_submits(assignment: AssignedTask):
                 score = EvaluationResult(submit.pipeline_path()).test_score()
                 result['passed_tests'] = score[0]
                 result['total_tests'] = score[1]
-
+        
         results.append((submit, result))
     return results
 
@@ -184,11 +255,11 @@ def submit_assign_points(request, submit_id):
 
     if submit.assigned_points != points:
         notify.send(
-            sender=request.user,
-            recipient=[submit.student],
-            verb='assigned points to',
-            action_object=submit,
-            important=True,
+                sender=request.user,
+                recipient=[submit.student],
+                verb='assigned points to',
+                action_object=submit,
+                important=True,
         )
 
     submit.assigned_points = points
@@ -215,8 +286,7 @@ def build_score_csv(assignments, filename):
             if login not in result:
                 result[login] = {'LOGIN': login}
 
-            result[login][assignment.task.name] = record[
-                'assigned_points'] if 'assigned_points' in record else 0
+            result[login][assignment.task.name] = record['assigned_points'] if 'assigned_points' in record else 0
 
     with io.StringIO() as out:
         w = csv.DictWriter(out, fieldnames=header)
@@ -225,54 +295,59 @@ def build_score_csv(assignments, filename):
         for login, row in result.items():
             w.writerow(row)
 
-        return file_response(out.getvalue(), filename, "text/csv")
-
-
-def build_edison_task_score_csv(student_points: Dict, filename: str):
-    """
-    Build CSV file importable by Edison system.
-    Edison requires delimiter set to `;`.
-    """
-    with io.StringIO() as out:
-        w = csv.writer(out, delimiter=';')
-        for student, points in student_points.items():
-            w.writerow([student.username, points])
-
-        return file_response(out.getvalue(), filename, "text/csv")
-
+        response = HttpResponse(out.getvalue(), 'text/csv')
+        response['Content-Disposition'] = f'attachment; filename="{unidecode(filename)}"'
+        return response
 
 @user_passes_test(is_teacher)
-def download_csv_per_assignment(request, assignment_id: int):
+def download_csv_per_task(request, assignment_id: int):
     assigned_task = AssignedTask.objects.get(pk=assignment_id)
     csv_filename = f"{assigned_task.task.sanitized_name()}_{assigned_task.clazz.day}{assigned_task.clazz.time:%H%M}.csv"
     return build_score_csv([assigned_task], csv_filename)
 
-
-@user_passes_test(is_teacher)
-def download_csv_per_task(request, task_id: int):
-    """
-    Students without a submit are excluded.
-    """
-    task = get_object_or_404(Task, pk=task_id)
-    submits = statistics.get_task_submits(task)
-    student_points = statistics.get_student_points(submits)
-
-    return build_edison_task_score_csv(student_points, f"{task.sanitized_name()}.csv")
-
-
 @user_passes_test(is_teacher)
 def download_csv_per_class(request, class_id: int):
     clazz = Class.objects.get(pk=class_id)
-    return build_score_csv(clazz.assignedtask_set.all(),
-                           f"{clazz.subject.abbr}_{clazz.day}{clazz.time:%H%M}.csv")
+    return build_score_csv(clazz.assignedtask_set.all(), f"{clazz.subject.abbr}_{clazz.day}{clazz.time:%H%M}.csv")
 
 
 @user_passes_test(is_teacher)
 def all_tasks(request, **kwargs):
-    return render(request, 'web/all_tasks.html', {
-        'tasks': Task.objects.filter(**kwargs).order_by('-id'),
-        'subjects': Subject.objects.all(),
-    })
+    search_term = request.GET.get('search')
+    if search_term:
+        tasks = Task.objects.filter(name__icontains=search_term, **kwargs).order_by('-id')
+    else:
+        tasks = Task.objects.filter(**kwargs).order_by('-id')
+
+    filter_type = request.GET.get('filter')
+
+    if filter_type == 'lessons':
+        tasks = tasks.filter(
+        Q(name__istartswith='lesson') |
+        Q(name__istartswith='bonus') |
+        Q(name__istartswith='du') |
+        Q(name__istartswith='doma') |
+        Q(name__istartswith='cv')
+        )
+    elif filter_type == 'tests':
+        tasks = tasks.filter(
+        Q(name__istartswith='test') |
+        Q(name__istartswith='pisemka') |
+        Q(name__istartswith='písemka')
+        )
+    elif filter_type == 'projects':
+        tasks = tasks.filter(
+        Q(name__istartswith='project') |
+        Q(name__istartswith='projekt')
+        )
+
+    context = {
+    'tasks': tasks,
+    'subjects': Subject.objects.all().order_by('abbr').distinct('abbr'),
+    'filter_type': filter_type # Add the filter type to the context
+    }
+
+    return render(request, 'web/all_tasks.html', context)
 
 
 @user_passes_test(is_teacher)
@@ -286,7 +361,6 @@ def reevaluate(request, submit_id):
     submit.jobid = evaluate_submit(request, submit).id
     submit.save()
     return redirect(request.META.get('HTTP_REFERER', reverse('submits')) + "#result")
-
 
 @user_passes_test(is_teacher)
 def bulk_import(request):
